8a9317a3e297c368bdfaf9411bfaaf35eab7ad4d
aprabha7 <aprabharam@gmail.com>
2026-04-16 14:42:50 +0200

    Modifying game - incomplete

--- script.js
+++ script.js
@@ -15,6 +15,28 @@ function playRound(playerSelection, computerSelection) {
     if(!['ROCK', 'PAPER', 'SCISSORS'].includes(playerChoice)) {
         return "Invalid player entry."
     }
-    
-    return "You Lose! FILL UP"
+
+    return `You Lose! FILL UP ${playerChoice}`
 }
+
+// const playerSelection = 'Rock';
+// const computerSelection = computerPlay();
+// console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for(let round = 0; round < 5; round++) {
+        let computerSelection = computerPlay();
+        console.log("Computer chose:", computerSelection);
+        let playerSelection = prompt("Hello! Enter Rock, Paper or Scissors to play the game");
+        let result = playRound(playerSelection, computerSelection);
+
+        console.log(playRound(playerSelection, computerSelection));
+
+        console.log("Winner of the round is - ")
+
+    }
+}
+
+game();
